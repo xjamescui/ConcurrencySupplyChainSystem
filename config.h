0e33d1f8d812abfdb36eed9bca3161843d84d195
jqcui <jqcui@student.cs.uwaterloo.ca>
2015-04-01 20:38:32 -0400

    init commit

---
+++ config.h
@@ -0,0 +1,23 @@
+//                              -*- Mode: C++ -*- 
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+struct ConfigParms {
+    unsigned int sodaCost;				// MSRP per bottle
+    unsigned int numStudents;				// number of students to create
+    unsigned int maxPurchases;				// maximum number of bottles a student purchases
+    unsigned int numVendingMachines;			// number of vending machines
+    unsigned int maxStockPerFlavour;			// maximum number of bottles of each flavour stocked
+    unsigned int maxShippedPerFlavour;			// number of bottles of each flavour in a shipment
+    unsigned int timeBetweenShipments;			// length of time between shipment pickup
+    unsigned int parentalDelay;				// length of time between cash deposits
+    unsigned int numCouriers;				// number of couriers in the pool
+}; // ConfigParms
+
+void processConfigFile( const char *configFile, ConfigParms &cparms );
+
+#endif // __CONFIG_H__
+
+// Local Variables: //
+// compile-command: "make" //
+// End: //
