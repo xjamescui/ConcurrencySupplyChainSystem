4072c9dbaa57ab8610a07309829d3c71cc92452b
Yu Wei Pan <david.pan2@gmail.com>
2015-04-03 17:24:55 -0400

    Added parent (tested)

--- testParent.cc
+++ testParent.cc
@@ -1,4 +1,9 @@
+#include "MPRNG.h"
+#include "printer.h"
+#include "bank.h"
 #include "parent.h"
+
+MPRNG g_randGenerator(getpid());
 
 void uMain::main() {
     const unsigned int numStudents = 5;
@@ -6,4 +11,5 @@ void uMain::main() {
     Printer prt(numStudents, 0, 0);
     Bank bank(numStudents);
     Parent parent( prt, bank, numStudents, parentalDelay );
+    yield(200);
 }
