4c506899832b57417e5bccd2b6bcd914729fc0c8
Yu Wei Pan <david.pan2@gmail.com>
2015-04-04 13:21:39 -0400

    WatcardOffice

--- watcardOffice.cc
+++ watcardOffice.cc
@@ -1,9 +1,45 @@
 #include "watcardOffice.h"
 
+WATCardOffice::WATCardOffice( Printer &prt, Bank &bank, unsigned int numCouriers ) :
+        prt( prt ),
+        bank( bank ),
+        NUM_COURIERS( numCouriers ) {
+}
 
-WATCardOffice::WATCardOffice( Printer &prt, Bank &bank, unsigned int numCouriers ){}
-WATCard::FWATCard WATCardOffice::create( unsigned int sid, unsigned int amount ){}
-WATCard::FWATCard WATCardOffice::transfer( unsigned int sid, unsigned int amount, WATCard *card ){}
-WATCardOffice::Job* WATCardOffice::requestWork(){}
+WATCard::FWATCard WATCardOffice::create( unsigned int sid, unsigned int amount ) {
+    Args args( sid, amount );
+    Job * job = new Job( args );
+    jobs.push( job );
+    cond.signal();
+    return job->result;
+}
 
-void WATCardOffice::main(){}
+WATCard::FWATCard WATCardOffice::transfer( unsigned int sid, unsigned int amount, WATCard *card ){
+    Args args( sid, amount, card );
+    Job * job = new Job( args );
+    jobs.push( job );
+    cond.signal();
+    return job->result;
+}
+
+WATCardOffice::Job* WATCardOffice::requestWork() {    
+    if ( jobs.empty() ) {
+	// Wait until there is a job.
+	cond.wait();
+    }
+
+    Job * job = jobs.front();
+    jobs.pop();
+    return job;
+}
+
+void WATCardOffice::main() {
+    Courier couriers[ NUM_COURIERS ];
+
+    while ( true ) {
+	_Accept( ~WATCardOffice ) {
+	    break;
+	} or _Accept( requestWork, create, transfer ) {
+	}
+    }
+}
--- watcardOffice.h
+++ watcardOffice.h
@@ -4,20 +4,44 @@
 #include "watcard.h"
 #include "printer.h"
 #include "bank.h"
+#include <queue>
+
+using namespace std;
 
 _Task WATCardOffice {
-    typedef int Args; // TODO: change this, this is temp just to make sure make works
+    Printer & prt;
+    Bank & bank;
+    const unsigned int NUM_COURIERS;
+    uCondition cond;
+    struct Args {
+	unsigned int sid;
+	unsigned int amount;
+	WATCard * card;
+	Args( unsigned int sid, unsigned int amount ) : sid( sid ), amount( amount ), card( NULL ) {}
+	Args( unsigned int sid, unsigned int amount, WATCard * card ) : 
+	        sid( sid ), 
+		amount( amount ), 
+		card( card ) {
+	}
+    };
+
     struct Job {                           // marshalled arguments and return future
         Args args;                         // call arguments (YOU DEFINE "Args")
         WATCard::FWATCard result;          // return future
         Job( Args args ) : args( args ) {}
     };
-    _Task Courier { };                 // communicates with bank
+
+    queue<Job *> jobs; 
+
+    _Task Courier {                        // communicates with bank
+	void main();
+    };                 
 
     void main();
   public:
     _Event Lost {};                        // lost WATCard
     WATCardOffice( Printer &prt, Bank &bank, unsigned int numCouriers );
+    ~WATCardOffice();
     WATCard::FWATCard create( unsigned int sid, unsigned int amount );
     WATCard::FWATCard transfer( unsigned int sid, unsigned int amount, WATCard *card );
     Job *requestWork();
