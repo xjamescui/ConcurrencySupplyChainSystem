822a02ad8c54b46e5b58c50a58ffbda93e893676
Yu Wei Pan <david.pan2@gmail.com>
2015-04-05 18:19:42 -0400

    Finished printer (untested)

--- printer.cc
+++ printer.cc
@@ -1,9 +1,203 @@
 #include "printer.h"
+#include <iostream>
 
-Printer::Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers ){}
-void Printer::print( Kind kind, char state ){}
-void Printer::print( Kind kind, char state, int value1 ){}
-void Printer::print( Kind kind, char state, int value1, int value2 ){}
-void Printer::print( Kind kind, unsigned int lid, char state ){}
-void Printer::print( Kind kind, unsigned int lid, char state, int value1 ){}
-void Printer::print( Kind kind, unsigned int lid, char state, int value1, int value2 ){}
+void Printer::printHeadings() {
+    // Print column headers.
+    cout << "Parent\tWATOff\tNames\tTruck\tPlant";
+
+    for ( unsigned int id = 0; id < NUM_STUDENTS; ++id ) {
+	cout << "\t" << "Stud" << id;
+    }
+
+    for ( unsigned int id = 0; id < NUM_VENDING_MACHINES; ++id ) {
+	cout << "\t" << "Mach" << id;
+    }
+
+    for ( unsigned int id = 0; id < NUM_COURIERS; ++id ) {
+	cout << "\t" << "Cour" << id ;
+    }
+
+    cout << endl;
+
+    const unsigned int numColumns = 5 + NUM_STUDENTS + NUM_VENDING_MACHINES + NUM_COURIERS;
+
+    for ( unsigned int i = 0; ; ++i ) {
+	cout << "*******";
+
+	if ( i == numColumns - 1 ) {
+	    // Last column.  Don't print the trailing tab.
+	    break;
+	}
+
+	cout << "\t";
+    }
+
+    cout << endl;
+}
+
+Printer::Printer( 
+	unsigned int numStudents, 
+	unsigned int numVendingMachines, 
+	unsigned int numCouriers ) :
+        NUM_STUDENTS( numStudents ),
+        NUM_VENDING_MACHINES( numVendingMachines )
+        NUM_COURIERS( numCouriers ),
+        isBufferEmpty( true ) {
+    printHeadings();
+
+    for ( int kind = Parent; kind <= BottlingPlant; ++kind ) {
+        // There is only one of each of parent, watcard office, name server, truck, 
+        // and bottling plant.	
+	stateInfos[kind] = new StateInfo[1];
+    }
+
+    stateInfos[Student] = new StateInfo[NUM_STUDENTS];
+    stateInfos[Vending] = new StateInfo[NUM_VENDING_MACHINES];
+    stateInfos[Courier] = new StateInfo[NUM_COURIERS];
+}
+
+Printer::~Printer() {
+    for ( int i = 0; i < NUM_KINDS; ++i ) {
+	delete [] stateInfos[i];
+    }
+}
+
+void Printer::flush() {
+    if ( isBufferEmpty ) {
+	// Don't print anything if the buffer is empty.
+	return;
+    }
+
+    for ( int kind = Parent; kind <= BottlingPlant; ++kind ) {
+	// Print columns corresponding to kinds that have only 1 object per kind.
+	stateInfos[kind][0].flushState();
+        cout << "\t";
+    }
+
+    // Print student columns.
+    for ( int lid = 0; lid < NUM_STUDENTS; ++lid ) {
+        stateInfos[Student][lid].flushState();	
+	cout << "\t";
+    }
+
+    // Print vending machine columns.
+    for ( int lid = 0; lid < NUM_VENDING_MACHINES; ++lid ) {
+        stateInfos[Vending][lid].flushState();	
+	cout << "\t";
+    }
+
+    // Print couriers.
+    for ( int lid = 0; ; ++lid ) {
+        stateInfos[Courier][lid].flushState();
+
+	if ( lid == NUM_COURIERS - 1 ) {
+	    // Last courier.  Don't print the tab.
+	    break;
+	}
+
+       	cout << "\t";	
+    }
+
+    cout << endl; 
+
+    isBufferEmpty = true;
+}
+
+void Printer::printFinish( Kind kind, unsigned int lid ) {
+    for ( int k = Parent; k <= BottlingPlant; ++k ) {
+	// Print columns corresponding to kinds that have only 1 object per kind.
+	if ( kind == k ) {
+	    cout << "F";
+	} else {
+	    cout << "...";
+	}
+
+        cout << "\t";
+    }
+
+    // Print student columns.
+    for ( int id = 0; id < NUM_STUDENTS; ++id ) {
+	if ( kind == Student && lid == id ) {
+	    cout << "F";
+	} else {
+	    cout << "...";
+	}
+
+	cout << "\t";
+    }
+
+    // Print vending machine columns.
+    for ( int id = 0; id < NUM_VENDING_MACHINES; ++id ) {
+	if ( kind == Vending && lid == id ) {
+	    cout << "F";
+	} else {
+	    cout << "...";
+	}
+
+	cout << "\t";
+    }
+
+    // Print couriers.
+    for ( int id = 0; ; ++id ) {
+	if ( kind == Courier && lid == id ) {
+	    cout << "F";
+	} else {
+	    cout << "...";
+	}
+
+	if ( id == NUM_COURIERS - 1 ) {
+	    // Last courier.  Don't print the tab.
+	    break;
+	}
+
+       	cout << "\t";	
+    }
+
+    cout << endl; 
+}
+
+void Printer::print( Kind kind, unsigned int lid, char state, vector<int> values ) {
+    if ( state == 'F' || ! stateInfos[kind][lid].isEmpty ) {
+	// Either an object finished or about to overwrite a column.  
+	// Flush buffer.
+	flush();
+    }
+
+    if ( state == 'F' ) {	
+	printFinish( kind, lid );
+    } else {
+	isBufferEmpty = false;
+	stateInfos[kind][lid].setState( state, values );
+    }
+}
+
+void Printer::print( Kind kind, char state ) {
+    print( kind, 0, state );
+}
+
+void Printer::print( Kind kind, char state, int value1 ) {
+    print( kind, 0, state, value1 );
+}
+
+void Printer::print( Kind kind, char state, int value1, int value2 ) {
+    print( kind, 0, state, value1, value2 );
+}
+
+void Printer::print( Kind kind, unsigned int lid, char state ) { 
+    // Empty vector of values.
+    vector<int> values;
+    print( kind, lid, state, values );
+}
+
+void Printer::print( Kind kind, unsigned int lid, char state, int value1 ) {
+    // Vector with a single element: value1.
+    vector<int> values( 1, value1 );
+    print( kind, lid, state, values );
+}
+
+void Printer::print( Kind kind, unsigned int lid, char state, int value1, int value2 ) {
+    // Vector with elements value1 and value2.
+    vector<int> values( 1, value1 );
+    values.push_back( value2 );
+    print( kind, lid, state, values );
+}
--- printer.h
+++ printer.h
@@ -1,11 +1,63 @@
 #ifndef __PRINTER_H__
 #define __PRINTER_H__
 
+#include <vector>
+#include <string>
+
+using namespace std;
+
 /* _Monitor / _Cormonitor Printer { */
 _Monitor Printer {
+    const static unsigned int NUM_KINDS = 8;
+    const unsigned int NUM_STUDENTS;
+    const unsigned int NUM_VENDING_MACHINES;
+    const unsigned int NUM_COURIERS;
+    struct StateInfo {
+	bool isEmpty;        // Set to true to indicate empty state.  
+	char state;
+	vector<int> values;
+	StateInfo()  : isEmpty( true ) {}
+	void setState( char state, vector<int> values ) {
+	    this->isEmpty = false;
+	    this->state = state;
+	    this->values = values;
+	} 
+
+	void flushState() {
+	    if ( isEmpty ) {
+		return;
+	    }
+
+	    cout << state;
+
+	    for ( vector<int>::iterator it = values.begin() ; it != values.end(); ++it ) {
+		if ( it != values.begin() ) {
+		    cout << ",";
+		}
+
+		cout << *it;
+	    }
+
+	    isEmpty = true;
+	}
+    };
+
+    // Each array element here is an array corresponding to a specific kind.  
+    // E.g. The array element corresponding to parent is an array of size 1
+    //      since there is only one parent.
+    //      But the array element corresponding to student is an array of size
+    //      NUM_STUDENTS.
+    StateInfo * stateInfos[NUM_KINDS];   
+    bool isBufferEmpty;
+
+    void printHeadings();
+    void flush();
+    void printFinish( Kind kind, unsigned int lid );
+    void print( Kind kind, unsigned int lid, char state, vector<int> values );
   public:
     enum Kind { Parent, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
     Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
+    ~Printer();
     void print( Kind kind, char state );
     void print( Kind kind, char state, int value1 );
     void print( Kind kind, char state, int value1, int value2 );
